a21ac0ce1598ce64f9a409a6a86851ec56d5aa9d
Jake Carnevale <jake.carnevale@realtruck.com>
2023-12-06 15:37:31 -0600

    Completed day 3 - part 1

---
+++ src/day3-gear-ratios/part1.ts
@@ -0,0 +1,74 @@
+import fs from "fs";
+import path from "path";
+import readline from "readline";
+
+interface Game {
+  id: number;
+  rounds: Round[];
+}
+
+interface Round {
+  blue?: number;
+  green?: number;
+  red?: number;
+}
+
+export function gearRatiosPart1() {
+  const filepath = path.resolve(__dirname, "input.txt");
+
+  const rows = fs
+    .readFileSync(filepath, "utf-8")
+    .split("\n")
+    // .slice(0, 2)
+    .map((row) => {
+      const numbers = [...row.matchAll(/\d+/g)].map((match) => {
+        const value = match[0];
+        const length = value.length;
+        const index = match.index ?? 0;
+
+        const start = index === 0 ? index : index - 1;
+        const end = index + length;
+
+        return {
+          index,
+          start,
+          end,
+          length,
+          value: +value,
+        };
+      });
+
+      return {
+        row,
+        numbers,
+      };
+    });
+
+  const partNumbers = rows
+    .map((row, index) => {
+      const previousRow = rows[index - 1]?.row;
+      const nextRow = rows[index + 1]?.row;
+
+      return row.numbers.filter(
+        ({ start, end }) =>
+          checkRowForSymbol(previousRow, start, end) ||
+          checkRowForSymbol(row.row, start, end) ||
+          checkRowForSymbol(nextRow, start, end)
+      );
+    })
+    .flat()
+    .map(({ value }) => value);
+
+  // console.log(partNumbers);
+  console.log(partNumbers.reduce((acc, partNumber) => acc + partNumber, 0));
+}
+
+function checkRowForSymbol(
+  row: string | undefined,
+  start: number,
+  end: number
+) {
+  if (!row) return false;
+  const rowBlock = row.substring(start, end + 1);
+  return rowBlock.search(/[^\d.]/g) > -1;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // import { trebuchetPart1 } from "./day1-trebuchet/part1";
 // import { trebuchetPart2 } from "./day1-trebuchet/part2";
 // import { cubeConundrumPart1 } from "./day2-cube-conundrum/part1";
-import { cubeConundrumPart2 } from "./day2-cube-conundrum/part2";
+// import { cubeConundrumPart2 } from "./day2-cube-conundrum/part2";
+import { gearRatiosPart1 } from "./day3-gear-ratios/part1";
 
-cubeConundrumPart2();
+gearRatiosPart1();
